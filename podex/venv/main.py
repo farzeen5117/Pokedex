5d76a593bb3c2cb16409e756d342056f9f95142a
farzeen5117 <96460343+farzeen5117@users.noreply.github.com>
2022-05-01 12:27:20 -0500

    commit2

--- podex/venv/main.py
+++ podex/venv/main.py
@@ -2,38 +2,39 @@ from flask import Flask
 import requests
 import pprint as pp
 
-# name, base exp, abilities (??), base_stat, sprites, official-artwork(front-default) (??)
+# name, base exp, abilities (??), sprites, official-artwork(front-default) (??)
 
 pokeurl = "https://pokeapi.co/api/v2/pokemon/"
-params = {'limit': 100}
+#params = {'limit': 100}
 
 def get_pokemon():
     res = requests.get(pokeurl)
     return res
 
-def get_pokemon_dict(url):
-    res = requests.get(url)
-    print(pp.pprint((res.json())))
+#def get_pokemon_dict(url):
+#    res = requests.get(url)
+#    print(pp.pprint((res.json())))
 
-def filter_poke_data():
-    for offset in range(0, 1000, 100):
-        params['offset'] = offset
-        response = requests.get(pokeurl, params=params)
-        if response.status_code != 200: 
-            print(response.text)
-        else:
-            data = response.json()
-            for item in data['results']:
-                print(item['name'])
+def get_pokemon_name():
+    response = get_pokemon()
+    if response: 
+        poke_dict = response.json()
+        for item in poke_dict['results']:
+            spec_poke_url = f"https://pokeapi.co/api/v2/pokemon/{item['name']}/"
+            spec_poke_res = requests.get(spec_poke_url)
+            spec_poke_dict = spec_poke_res.json()
+            print(f"{item['name']}: {item['url']}")
+    else:
+        print("Error")
 
 if __name__ == "__main__":
-    res = get_pokemon()
-    if res:
-        print(res.json())
-        print("\nsuccessful\n")
-    else:
-        print("something went wrong :(")
-    result = res.json()
-    for pokemon in result["results"]:
-        get_pokemon_dict(pokemon["url"])
-    filter_poke_data()
+    #res = get_pokemon()
+    #if res:
+    #    print(res.json())
+    #    print("\nsuccessful\n")
+    #else:
+    #    print("something went wrong :(")
+    #result = res.json()
+    #for pokemon in result["results"]:
+    #    get_pokemon_dict(pokemon["url"])
+    get_pokemon_name()
