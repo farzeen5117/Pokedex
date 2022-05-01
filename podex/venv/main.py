c7c0f58e9eb12d0c4f7230802544f8f9c3f04eb1
farzeen5117 <96460343+farzeen5117@users.noreply.github.com>
2022-05-01 11:41:25 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/farzeen5117/Pokedex""
    This reverts commit f6463f2628e2f081aa65f4accc556eec39b933fa.

--- podex/venv/main.py
+++ podex/venv/main.py
@@ -1,21 +1,40 @@
 from flask import Flask
 import requests
-import json
+import pprint as pp
 
-# name, base exp, abilities (??), base_stat, sprites, official-artwork(front-default) 
+# name, base exp, abilities (??), base_stat, sprites, official-artwork(front-default) (??)
+
+pokeurl = "https://pokeapi.co/api/v2/pokemon/"
+params = {'limit': 100}
 
 def get_pokemon():
-    req = requests.get("https://pokeapi.co/api/v2/pokemon/")
-    return req
+    res = requests.get(pokeurl)
+    return res
 
 def get_pokemon_dict(url):
-    req = requests.get(url)
-    print(json.dumps(req.json(), indent=2))
-    
+    res = requests.get(url)
+    print(pp.pprint((res.json())))
+
+def filter_poke_data():
+    for offset in range(0, 1000, 100):
+        params['offset'] = offset
+        response = requests.get(pokeurl, params=params)
+        if response.status_code != 200: 
+            print(response.text)
+        else:
+            data = response.json()
+            pp.pprint(data)
+            for item in data['results']:
+                print(item['name'])
 
 if __name__ == "__main__":
-    req = get_pokemon()
-    print(req.json())
-    result = req.json()
+    res = get_pokemon()
+    if res:
+        print(res.json())
+        print("\nsuccessful\n")
+    else:
+        print("something went wrong :(")
+    result = res.json()
     for pokemon in result["results"]:
         get_pokemon_dict(pokemon["url"])
+    filter_poke_data()
