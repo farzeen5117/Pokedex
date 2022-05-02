5c8b89d160e12c29d37d5e7d07e771a234a83afe
farzeen5117 <96460343+farzeen5117@users.noreply.github.com>
2022-05-02 10:24:23 -0500

    commit

--- podex/venv/main.py
+++ podex/venv/main.py
@@ -5,7 +5,7 @@ import pprint as pp
 # name, base exp, abilities (??), official-artwork(front-default) (??)
 
 pokeurl = "https://pokeapi.co/api/v2/pokemon/"
-#params = {'limit': 100}
+params = {'limit': 100}
 
 def get_pokemon():
     res = requests.get(pokeurl)
@@ -16,19 +16,21 @@ def get_pokemon():
 #    print(pp.pprint((res.json())))
 
 def get_pokemon_name():
-    response = get_pokemon()
-    if response:  
-        poke_dict = response.json()
-        for item in poke_dict['results']:
-            spec_poke_url = f"https://pokeapi.co/api/v2/pokemon/{item['name']}/"
-            spec_poke_res = requests.get(spec_poke_url)
-            if spec_poke_res:
-                spec_poke_dict = spec_poke_res.json()
-            else:
-                print("Error in retrieveing specific pokemon")
-            print(f"{spec_poke_dict['id']}: {item['name']}, {item['url']}, species: {spec_poke_dict['species']}, height: {spec_poke_dict['height']}, weight: {spec_poke_dict['weight']}, base exp: {spec_poke_dict['base_experience']}")
-    else:
-        print("Error in get_pokemon()")
+    for offset in range(0, 1000, 100):
+        params['offset'] = offset
+        response = requests.get(pokeurl, params=params)
+        if response:  
+            poke_dict = response.json()
+            for item in poke_dict['results']:
+                spec_poke_url = f"https://pokeapi.co/api/v2/pokemon/{item['name']}/"
+                spec_poke_res = requests.get(spec_poke_url)
+                if spec_poke_res:
+                    spec_poke_dict = spec_poke_res.json()
+                else:
+                    print("Error in retrieveing specific pokemon")
+                print(f"{spec_poke_dict['id']}: {item['name']}, {item['url']}, height: {spec_poke_dict['height']}, weight: {spec_poke_dict['weight']}, base exp: {spec_poke_dict['base_experience']}")
+        else:
+            print("Error in get_pokemon()")
 
 if __name__ == "__main__":
     get_pokemon_name()
