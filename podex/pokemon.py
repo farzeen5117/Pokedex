cff081915e43fa601e9d19b2faa058f97d0802c9
farzeen5117 <96460343+farzeen5117@users.noreply.github.com>
2022-05-19 17:57:48 -0500

    commit

--- podex/pokemon.py
+++ podex/pokemon.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, request
 import requests
 
 pokeurl = "https://pokeapi.co/api/v2"
@@ -9,7 +9,13 @@ def get_first_pokemon():
     pass
 
 @pokebp.route("/<int:id>")
+@pokebp.route("/<string:id>")
 def get_pokemon(id):
     res = requests.get(f"{pokeurl}/pokemon/{id}")
     data = res.json()
     return render_template("pokemon/pokemon.html", len = len(data.get("types")), pokemon=data)
+
+@pokebp.route("/handle_pokemon", methods=["POST"])
+def handle_pokemon():
+    pokemon = request.form["pokemon"].lower()
+    return redirect(url_for("pokemon.get_pokemon", id=pokemon))
